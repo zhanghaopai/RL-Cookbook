c9c2c60d8a95236c2eb512cc3074b95799757f93
Patrick <zhanghaopai@outlook.com>
2024-06-26 09:08:42 +0800

    提交新版本

--- 11TRPO/11TRPO.py
+++ 11TRPO/11TRPO.py
@@ -11,9 +11,10 @@ from tqdm import tqdm
 learning_rate = 1e-3
 num_episodes = 1000
 gamma = 0.98
+lam = 0.95
 kl_constraint = 0.00005
-env_name = "Pendulum-v0"
-play_mode = "human"
+env_name = "Pendulum-v1"
+play_mode = "rgb_array"
 
 
 # 策略网络
@@ -61,7 +62,11 @@ class Trajectory:
 
 
 class TRPO:
-    def __init__(self, state_dim, action_dim, learning_rate, gamma, kl_constraint):
+    '''
+    TRPO 算法实现
+    '''
+
+    def __init__(self, state_dim, action_dim, learning_rate, gamma, lam, kl_constraint):
         '''
         TRPO算法初始化
         :param state_dim:
@@ -72,12 +77,12 @@ class TRPO:
         '''
         # 策略网络
         self.policy_net = PolicyNet(state_dim, action_dim)
-        # 策略网络优化不需要优化器，
-        # todo：为什么不需要优化器
+        # 策略网络优化不需要优化器
         self.value_net = ValueNet(state_dim)
         self.value_optimizer = torch.optim.Adam(self.value_net.parameters(), lr=learning_rate)
         # reward折扣因子
         self.gamma = gamma
+        self.lam = lam
         self.kl_constraint = kl_constraint  # KL距离最大限制
 
     def take_action(self, state):  # 根据动作概率分布随机采样
@@ -106,7 +111,7 @@ class TRPO:
         ratio = torch.exp(log_probs - old_log_probs)
         return torch.mean(ratio * advantage)
 
-    def GAE(self, gamma, lbd, td_delta):
+    def gae(self, gamma, lam, td_delta):
         '''
         广义优势估计
         :return:
@@ -115,84 +120,67 @@ class TRPO:
         advantage_list = []
         advantage = 0.0
         for delta in reversed(td_delta):
-            advantage = gamma * lbd * advantage + delta
+            advantage = gamma * lam * advantage + delta
             advantage_list.append(advantage)
         advantage_list.reverse()
         return torch.tensor(advantage_list, dtype=torch.float)
 
-    def policy_gradient(self):
-        pass
-
-    def hessian_matrix_vector_product(self,
-                                      states,
-                                      old_action_dists,
-                                      vector,
-                                      damping=0.1):
-        mu, std = self.policy_net(states)
-        new_action_dists = torch.distributions.Normal(mu, std)
-        kl = torch.mean(
-            torch.distributions.kl.kl_divergence(old_action_dists,
-                                                 new_action_dists))
-        kl_grad = torch.autograd.grad(kl,
-                                      self.policy_net.parameters(),
-                                      create_graph=True)
-        kl_grad_vector = torch.cat([grad.view(-1) for grad in kl_grad])
-        kl_grad_vector_product = torch.dot(kl_grad_vector, vector)
-        grad2 = torch.autograd.grad(kl_grad_vector_product,
-                                    self.actor.parameters())
-        grad2_vector = torch.cat(
-            [grad.contiguous().view(-1) for grad in grad2])
-        return grad2_vector + damping * vector
-
-    def conjugate_gradient(self, grad, states, old_action_dists):
-        x = torch.zeros_like(grad)
-        r = grad.clone()
-        p = grad.clone()
-        rdotr = torch.dot(r, r)
-        for i in range(10):
-            Hp = self.hessian_matrix_vector_product(states, old_action_dists,
-                                                    p)
-            alpha = rdotr / torch.dot(p, Hp)
+    def cg(self, Ax, b, cg_iter: 10):
+        '''
+        conjugate graient algorithm 共轭梯度法求解方程
+        :param Ax:
+        :param b:
+        :return:
+        '''
+        x = np.zeros_like(b)
+        r = b.copy()
+        p = r.copy()
+        rdotr = np.dot(r, r)
+        for _ in range(cg_iter):
+            z = Ax(p)
+            alpha = rdotr / np.dot(p, z)
             x += alpha * p
-            r -= alpha * Hp
-            new_rdotr = torch.dot(r, r)
-            if new_rdotr < 1e-10:
+            r -= alpha * z
+            new_rdotr = np.dot(r, r)
+            if new_rdotr < 1e-8:
                 break
             beta = new_rdotr / rdotr
             p = r + beta * p
             rdotr = new_rdotr
         return x
 
-    def update(self, transition_dict):
+    def update(self, trajectory):
         '''
         根据trajectory，优化策略网络
         :param transition_dict:
         :return:
         '''
-        # {s_1, a_1, r_1, ... , s_{t-1}, a_{t-1}, r_{t-1}}
-        reward_list = transition_dict['rewards']
-        state_list = transition_dict['states']
-        action_list = transition_dict['actions']
+        state_list = trajectory.state
+        action_list = trajectory.action
+        reward_list = trajectory.reward
+        next_state_list = trajectory.next_state
+        done_list = trajectory.done
 
-        G = 0
-        self.policy_optimizer.zero_grad()
-        for i in reversed(range(len(reward_list))):  # 从最后一步算起
-            # 获取reward
-            reward = reward_list[i]
-            # 获取state
-            state = torch.tensor([state_list[i]],
-                                 dtype=torch.float)
-            # 获取action
-            action = torch.tensor([action_list[i]]).view(-1, 1)
-            # 当前网络参数情况下，state状态算出来的action概率，即P(a|s),并将结果求对数
-            log_prob = torch.log(self.policy_net(state).gather(1, action))
-            # 用累计奖励值G作为Q(s,a)的无偏估计
-            G = self.gamma * G + reward
-            # 每一步的损失函数
-            loss = -log_prob * G
-            # 反向传播计算梯度
-            loss.backward()
-        self.policy_optimizer.step()  # 梯度下降
+        td_target = reward_list + self.gamma * self.value_net(next_state_list) * (1 - done_list)
+        td_delta = td_target - self.value_net(state_list)
+        advantage = self.gae(self.gamma, self.lam, td_delta)
+        action_dists = self.policy_net(state_list)
+
+        old_log_prob = action_dists.log_prob(action_list)
+        value_loss =torch.mean(F.mse_loss(action_dists, td_target.detach()))
+        # 清空value_net梯度
+        self.value_optimizer.zero_grad()
+        # 反向传播
+        value_loss.backward()
+        # 梯度下降
+        self.value_optimizer.step()
+
+
+        # 策略优化
+        surrogate = self.surrogate_loss(old_log_prob,
+                                        self.policy_net(state_list).log_prob(action_list),
+                                        advantage)
+        print(surrogate)
 
 
 def train(env, agent):
@@ -203,13 +191,7 @@ def train(env, agent):
                 # trajectory的总回报
                 episode_return = 0
                 # trajectory的字典
-                transition_dict = {
-                    'states': [],
-                    'actions': [],
-                    'next_states': [],
-                    'rewards': [],
-                    'dones': []
-                }
+                trajectory = Trajectory()
                 state = env.reset()
                 # 初始状态
                 state = state[0]
@@ -217,16 +199,13 @@ def train(env, agent):
                 # 玩一局游戏，得到一个trajectory，直到游戏结束
                 while not done:
                     action = agent.take_action(state)
-                    next_state, reward, done, _, _ = env.step(action)
-                    transition_dict['states'].append(state)
-                    transition_dict['actions'].append(action)
-                    transition_dict['next_states'].append(next_state)
-                    transition_dict['rewards'].append(reward)
-                    transition_dict['dones'].append(done)
+                    next_state, reward, done, _, _ = env.step([action])
+                    # 记录动作
+                    trajectory.push(state, action, reward, done, next_state)
                     state = next_state
                     episode_return += reward
                 return_list.append(episode_return)
-                agent.update(transition_dict)
+                agent.update(trajectory)
                 if (i_episode + 1) % 10 == 0:
                     pbar.set_postfix({
                         'episode':
@@ -245,13 +224,13 @@ def init_env(env_name, mode):
     '''
     env = gym.make(env_name, render_mode=mode)
     state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.n
+    action_dim = env.action_space.shape[0]
     return env, state_dim, action_dim
 
 
 def main():
     env, state_dim, action_dim = init_env(env_name, play_mode)
-    agent = TRPO(state_dim, action_dim, learning_rate, gamma, kl_constraint)
+    agent = TRPO(state_dim, action_dim, learning_rate, gamma, lam, kl_constraint)
     train(env, agent)
 
 
